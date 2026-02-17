7ccfa50295dcc7adb298c408b553f314a6e2fc4f
HirparaChitt <chitthirpara@gmail.com>
2026-02-17 15:37:56 +0530

    Update 02 code

--- 02/index.js
+++ 02/index.js
@@ -1,26 +1,116 @@
+console.log("test");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-const users = [
-  { id: 1, name: "Arjun", role: "student" },
-  { id: 2, name: "Priyesha", role: "mentor" }
-];
+const userss = [
 
-app.get("/users", (req, res) => {
-  res.status(200).json(users);
+    {
+        "att": '80',
+        "uid": 108551,
+        "total_sub": 14,
+        "bonus": '20',
+        "name": 'Rishi'
+    },
+    {
+        "att": '90',
+        "uid": 108554,
+        "total_sub": 14,
+        "bonus": '20',
+        "name": 'chirag'
+    },
+    {
+        "att": '70',
+        "uid": 108552,
+        "total_sub": 14,
+        "bonus": '20',
+        "name": 'ishi'
+    }]
+
+app.get("/user/:id", (req, res) => {
+    const userId = Number(req.params.id);
+    const user = userss.find(u => u.uid === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
 });
 
-app.get("/user/test/:user_id", (req, res) => {
-    console.log(req);
-    res.status(200).json(users);
+
+app.post("/user", (req, res) => {
+    const newUser = {
+        uid: req.body.uid,
+        name: req.body.title,
+        bonus: req.body.price
+    };
+
+    userss.push(newUser);
+
+    res.status(201).json({
+        message: "User created",
+        user: newUser
+    });
 });
 
 
-app.get("/", (req, res) => {
-  res.send("Express server is running");
+app.put("/users/:id", (req, res) => {
+    const userId = Number(req.params.id);
+    const index = userss.findIndex(u => u.uid === userId);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    userss[index] = {
+        uid: userId,
+        name: req.body.name,
+        bonus: req.body.bonus,
+        att: req.body.att,
+
+    };
+
+    res.status(200).json({
+        message: "User replaced",
+        user: userss[index]
+    });
 });
 
+// app.patch("/pro/:id", (req, res) => {
+//   const userId = Number(req.params.id);
+//   const user = data.find(u => u.id === userId);
+
+//   if (!user) {
+//     return res.status(404).json({ message: "User not found" });
+//   }
+
+//   if (req.body.title) user.title = req.body.title;
+//   if (req.body.price) user.price = req.body.price;
+
+//   res.status(200).json({
+//     message: "User updated",
+//     user
+//   });
+// });
+
+
+// app.delete("/proprer/:id", (req, res) => {
+//     const userId = Number(req.params.id);
+//     const index = data.findIndex(u => u.id === userId);
+
+//     if (index === -1) {
+//         return res.status(404).json({ message: "User not found" });
+//     }
+
+//     data.splice(index, 1);
+
+//     res.status(204).end();
+// });
+
+
+app.use(express.json());
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
